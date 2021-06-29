6920bb4fdc5e2e88f4b7d3613a272b0cf5002dfd
duanit <86536251+duanit@users.noreply.github.com>
2021-06-29 15:13:36 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,19 +1,45 @@
 //ทำการ import express เข้ามาใช้งาน โดยสร้างตัวแปร express ขึ้นมาเพื่อรับค่า
-const express = require('express')
+const express = require('express');
+const axios = require('axios');
+var forms = require('forms');
 //ทำการสร้าง Instance ของ express และสร้างตัวแปร app ขึ้นมาเพื่อรับค่า
-const app = express()
+const app = express();
+const path = require('path');
+const router = express.Router();
 //สร้างตัวแปร PORT ขึ้นมารับค่า port ในกรณีที่เราได้กำหนดไว้ใน environment ของเครื่อง
 //แต่ถ้าไม่ได้กำหนดไว้ เราจะใช้ค่า 8080 แทน
 const PORT = process.env.PORT || 8080
 //สร้าง route ขึ้นมา 1 ตัว โดยกำหนดให้ path คือ / หรือ index ของ host นั่นเอง
 //จากนั้นให้กำหนด response แสดงคำว่า Hello World
-app.get('/', (req, res) => res.send('Hello World'))
+// var fields = forms.fields;
+// var validators = forms.validators;
+ 
+// var reg_form = forms.create({
+//     username: fields.string({ required: true }),
+//     password: fields.password({ required: validators.required('You definitely want a password') }),
+//     confirm:  fields.password({
+//         required: validators.required('don\'t you know your own password?'),
+//         validators: [validators.matchField('password')]
+//     }),
+//     email: fields.email()
+// });
+
+// app.get('/', (req, res) => res.send(
+//     'Hello World1'
+//     ))
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+    //__dirname : It will resolve to your project folder.
+  });
+
 //run web server ที่เราสร้างไว้ โดยใช้ PORT ที่เรากำหนดไว้ในตัวแปร PORT
+
+app.use('/', router);
 app.listen(PORT, () => {
     //หากทำการ run server สำเร็จ ให้แสดงข้อความนี้ใน cmd หรือ terminal
     console.log(`Server is running on port : ${PORT}`);
 
-     console.log('Hello World');
+    console.log('Hello World');
     var cron = require('node-cron');
     var msTo = "Cc979fb358667360d869569c072a80196";
     var msMessage = "tesssst";
@@ -32,3 +58,4 @@ app.listen(PORT, () => {
 //ทำการ export app ที่เราสร้างขึ้น เพื่อให้สามารถนำไปใช้งานใน project อื่นๆ 
 //เปรียบเสมือนเป็น module ตัวนึง
 module.exports = app
+
