29a24a713773efc4e5e6db13cd3edb77b087e14f
duanit <86536251+duanit@users.noreply.github.com>
2021-06-29 23:31:22 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const axios = require('axios');
 var forms = require('forms');
+var moment = require('moment'); // require
 //ทำการสร้าง Instance ของ express และสร้างตัวแปร app ขึ้นมาเพื่อรับค่า
 const app = express();
 const path = require('path');
@@ -13,7 +14,7 @@ const PORT = process.env.PORT || 8080
 //จากนั้นให้กำหนด response แสดงคำว่า Hello World
 // var fields = forms.fields;
 // var validators = forms.validators;
- 
+
 // var reg_form = forms.create({
 //     username: fields.string({ required: true }),
 //     password: fields.password({ required: validators.required('You definitely want a password') }),
@@ -31,42 +32,77 @@ const fs = require('fs')
 const folderName = './test'
 
 try {
-  if (!fs.existsSync(folderName)) {
-    fs.mkdirSync(folderName)
-  }
+    if (!fs.existsSync(folderName)) {
+        fs.mkdirSync(folderName)
+    }
 } catch (err) {
-  console.error(err)
+    console.error(err)
 }
 
-const content = 'Some content!'
+// const content = 'Some content!'
 
-fs.writeFile('./test/test.txt', content, err => {
-  if (err) {
-    console.error(err)
-    return
-  }
-  //file written successfully
-  console.log("file written successfully");
+// fs.writeFile('./test/test.txt', content, err => {
+//     if (err) {
+//         console.error(err)
+//         return
+//     }
+//     //file written successfully
+//     console.log("file written successfully");
+// });
+
+// fs.readFile('./test/test.txt', 'utf8', (err, data) => {
+//     if (err) {
+//         console.error(err)
+//         return
+//     }
+//     console.log(data)
+// });
+
+
+// router.get('/',function(req,res){
+//     res.sendFile(path.join(__dirname+'/index.html'));
+//     //__dirname : It will resolve to your project folder.
+//   });
+//   router.get('/index',function(req,res){
+//     res.sendFile(path.join(__dirname+'/index.js'));
+//     //__dirname : It will resolve to your project folder.
+//   });
+app.get('/', (req, res) => {
+    // HTML Form
+    //     res.send(`<form action="/test" method="GET">
+    //     <label>Username</label><br>
+    //     <input type="text" name="username" placeholder="Enter Username"><br>
+    //     <label>Password</label><br>
+    //     <input type="password" name="pass" placeholder="Enter Password"><br><br>
+    //     <input type="submit">
+    // </form>`);
+    res.send(`<form action="/test" method="GET"><div class="form-group">
+          <label for="msDate">Date : </label>
+          <input type="datetime-local" class="form-control" id="msDate" name="msDate">
+        </div>
+        <div class="form-group">
+          <label for="msMessage"> Message : </label>
+          <input type="text" class="form-control" id="msMessage" name="msMessage">
+        </div><input type="submit"></form>`);
 });
 
-fs.readFile('./test/test.txt', 'utf8' , (err, data) => {
-    if (err) {
-      console.error(err)
-      return
-    }
-    console.log(data)
-  });
+app.get('/test', (req, res) => {
 
+    var _msDate = moment(req.query.msDate).format('YYYYMMDDHHmm');
 
-router.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/index.html'));
-    //__dirname : It will resolve to your project folder.
-  });
-  router.get('/index',function(req,res){
-    res.sendFile(path.join(__dirname+'/index.js'));
-    //__dirname : It will resolve to your project folder.
-  });
+    //Displaying the GET data in console
+    console.log(req.query);
+    res.send('Check the console');
+    fs.writeFile('./test/' + _msDate + '.txt', req.query.msMessage, err => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        //file written successfully
+        console.log("file written successfully");
+    });
 
+});
 //run web server ที่เราสร้างไว้ โดยใช้ PORT ที่เรากำหนดไว้ในตัวแปร PORT
 
 app.use('/', router);
@@ -79,19 +115,48 @@ app.listen(PORT, () => {
     var msTo = "Cc979fb358667360d869569c072a80196";
     var msMessage = "tesssst";
     var i = 0;
-    cron.schedule('0 01 16 * * *', function () {
-        if(i < 2){
-            console.log('i',i);
-            axios.post('https://pushlinems.herokuapp.com/PushMsline2.php', {"to": msTo, "messages": msMessage });
-         //   axios.post('http://ec2-13-213-4-106.ap-southeast-1.compute.amazonaws.com/api/PushMsline2.php', {"to": msTo, "messages": msMessage });
+    //cron.schedule('0 01 16 * * *', function () {
+    cron.schedule('* */1 * * * *', function () {
+        var _Datenow = moment().format('YYYYMMDDHHmm');
+        var _Datenowss = moment().format('YYYYMMDDHHmmss');
+        fs.stat('./test/' + _Datenow + '.txt', (err, stats) => {
+            if (err) {
+                //console.error(err)
+                return
+            }else{
+                fs.readFile('./test/' + _Datenow + '.txt', 'utf8', (err, data) => {
+                    if (err) {
+                        //console.error(err)
+                        return
+                    } else {
+                        axios.post('http://ec2-13-213-4-106.ap-southeast-1.compute.amazonaws.com/api/PushMsline2.php', { "to": msTo, "messages": data+'_'+moment().format('YYYYMMDDHHmmss') });
+                        console.log(data+'_'+moment().format('YYYYMMDDHHmmss'));
+                        const path = './test/' + _Datenow + '.txt';
+                        try {
+                            fs.unlinkSync(path)
+                            //file removed
+                        } catch (err) {
+                            console.error(err)
+                        }
+                    }
+    
+                });
+            }
+           
+        })
+
+        if (i < 2) {
+            console.log('i', i);
+            // axios.post('https://pushlinems.herokuapp.com/PushMsline2.php', {"to": msTo, "messages": msMessage });
+            //   axios.post('http://ec2-13-213-4-106.ap-southeast-1.compute.amazonaws.com/api/PushMsline2.php', {"to": msTo, "messages": msMessage });
         }
-       
-       // console.log('running a task every minute');
+
+        // console.log('running a task every minute');
         i++;
     }, {
         scheduled: true,
         timezone: "Asia/Bangkok"
-      });
+    });
 })
 //ทำการ export app ที่เราสร้างขึ้น เพื่อให้สามารถนำไปใช้งานใน project อื่นๆ 
 //เปรียบเสมือนเป็น module ตัวนึง
